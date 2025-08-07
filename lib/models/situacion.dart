f895dcb90d4cb6830a1a33cbf344dacfab94457b
CrisMonteiro23 <cristianjmonteiro@gmail.com>
2025-08-07 20:37:33 +0200

    Update situacion.dart

--- lib/models/situacion.dart
+++ lib/models/situacion.dart
@@ -1,15 +1,12 @@
 // lib/models/situacion.dart
-// Este archivo define la estructura de datos para una "Situación" o evento registrado en cancha.
-
 class Situacion {
-  String id; // Identificador único de la situación
-  DateTime timestamp; // Momento exacto en que se registró la situación
-  bool esAFavor; // True si es "Llegada a favor", False si es "Llegada en contra"
-  String tipoLlegada; // Tipo de llegada (ej: "Ataque Posicional", "5x4")
-  List<String> jugadoresEnCanchaIds; // IDs de los 5 jugadores que estaban en cancha
-  List<String> jugadoresEnCanchaNombres; // Nombres de los 5 jugadores (para fácil visualización)
+  String id;
+  DateTime timestamp;
+  bool esAFavor;
+  String tipoLlegada;
+  List<String> jugadoresEnCanchaIds;
+  List<String> jugadoresEnCanchaNombres;
 
-  // Constructor de la clase Situacion.
   Situacion({
     required this.id,
     required this.timestamp,
@@ -19,11 +16,10 @@ class Situacion {
     required this.jugadoresEnCanchaNombres,
   });
 
-  // Método para convertir un objeto Situacion a un formato JSON (Map).
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'timestamp': timestamp.toIso8601String(), // Guarda la fecha y hora como String
+      'timestamp': timestamp.toIso8601String(),
       'esAFavor': esAFavor,
       'tipoLlegada': tipoLlegada,
       'jugadoresEnCanchaIds': jugadoresEnCanchaIds,
@@ -31,11 +27,10 @@ class Situacion {
     };
   }
 
-  // Método de fábrica para crear un objeto Situacion desde un formato JSON (Map).
   factory Situacion.fromJson(Map<String, dynamic> json) {
     return Situacion(
       id: json['id'],
-      timestamp: DateTime.parse(json['timestamp']), // Convierte String a DateTime
+      timestamp: DateTime.parse(json['timestamp']),
       esAFavor: json['esAFavor'],
       tipoLlegada: json['tipoLlegada'],
       jugadoresEnCanchaIds: List<String>.from(json['jugadoresEnCanchaIds']),
